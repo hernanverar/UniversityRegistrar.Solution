63c83a4e0ba76013cb07b68dd20c1229442e946f
hernan verar <hernanverar@gmail.com>
2023-08-01 10:14:49 -0700

    This commit add project to start development of our App

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RegistrationApp.Models;
+
+namespace RegistrationApp.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Models/Stundent.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+
+
+namespace RegistrationApp.Models;
+{
+    public class Student
+    {
+    // public int StudentId { get; set; }
+    // public string StundentName { get; set; }
+    }
+}
